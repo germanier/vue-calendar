fb6070421125fbea3003d22b487f04bcf534946e
Marco Germanier <marco@mrgt.me>
2023-01-12 23:01:08 +0000

    clean: clean vue project

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+import PrimeVue from "primevue/config";
+
+import Button from "primevue/button";
+
 import "./assets/main.css";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+app.use(PrimeVue);
+app.mount("#app");
