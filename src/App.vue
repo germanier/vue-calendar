588694c22858cd7e0cc9c95905c5e6394593b310
Marco Germanier <marco@mrgt.me>
2023-01-12 23:53:09 +0000

    boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,44 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import { useToast } from "primevue/usetoast";
 
-<template></template>
+const arr = ["warn", "success", "info", "error"];
+const counter = ref<number>(0);
+const text_ = ref("Hello World");
+const toast = useToast();
+const greet = () => {
+  toast.add({
+    severity: arr[counter.value % 4],
+    summary: "Success",
+    detail: text_.value,
+    life: 30000,
+  });
+};
+</script>
 
-<style></style>
+<template>
+  <Toast></Toast>
+  <div class="container">
+    <span class="p-float-label">
+      <InputText id="username" type="text" v-model="text_" />
+      <label for="username">Username</label>
+    </span>
+    <Button
+      label="Submit"
+      @click="
+        greet();
+        counter++;
+      "
+      icon="pi pi-user"
+    ></Button>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,26 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+// PrimeVue
 import PrimeVue from "primevue/config";
 
+// primevue components
+import InputText from "primevue/inputtext";
 import Button from "primevue/button";
+import Toast from "primevue/toast";
+import ToastService from "primevue/toastservice";
 
-import "./assets/main.css";
+// Importing the CSS files
+import "primevue/resources/themes/vela-green/theme.css"; //theme
+import "primevue/resources/primevue.min.css"; //core css
+import "primeicons/primeicons.css"; //icons
+import "@/assets/css/app.css"; //custom global css
 
+// create the app
 const app = createApp(App);
-app.use(PrimeVue);
+app.use(PrimeVue, { ripple: true });
+app.use(ToastService);
+app.component("InputText", InputText);
+app.component("Button", Button);
+app.component("Toast", Toast);
 app.mount("#app");
